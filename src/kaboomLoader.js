af40ecfadbce944532e325d83cb47ecf11a3da29
jolobart <bartjoseph19@gmail.com>
2025-11-25 20:48:23 +0800

    Added snake and poison

--- src/kaboomLoader.js
+++ src/kaboomLoader.js
@@ -1,9 +1,14 @@
 import kaboom from "../lib/kaboom.mjs";
 
 export const scale = 2;
+
 export const k = kaboom({
-    width: 640,
-    height: 360,
+    width: 640 * scale,
+    height: 360 * scale,
+    scale,
     letterbox: true,
     global: false,
 });
+
+k.loadSprite("snake", "../assets/Snake.png");
+k.loadSprite("poison", "../assets/Poison.png")
--- src/main.js
+++ src/main.js
@@ -1,1 +1,51 @@
 import { k } from "./kaboomLoader.js";
+
+const SPEED = 400;
+const BULLET_SPEED = 600;
+
+const player = k.add([
+	k.sprite("snake"), 
+	k.pos(k.center())     
+]);
+
+console.log(  );
+
+const poison = k.add([
+	, 
+	k.pos(player.pos.x + 200, player.pos.y + 100)     
+]);
+
+function spawnPoison(px, py) {
+	k.add([
+        k.sprite("poison"),
+		k.area(),
+		k.pos(px, py),
+		k.anchor("center"),
+		k.move(k.UP, BULLET_SPEED),
+		k.offscreen({ destroy: true }),
+		"bullet"
+	]);
+}
+
+k.onKeyPress("space", () => {
+	spawnPoison(player.pos.x + 50, player.pos.y);
+});
+
+
+k.onKeyDown("a", () => {
+	player.move(-SPEED, 0)
+});
+
+k.onKeyDown("d", () => {
+	player.move(SPEED, 0)
+});
+
+k.onKeyDown("w", () => {
+	player.move(0, -SPEED)
+});
+
+k.onKeyDown("s", () => {
+	player.move(0, SPEED)
+});
+
+
